169b7b19424c16b7c5d3f7c5cad7899b2fc70ce6
Yulu <161650870+yulunez@users.noreply.github.com>
2024-09-24 09:45:08 -0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reto_MD</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div>
+        <div>
+            <h1>Iniciar Sesión</h1>
+            <p>¡Bienvenido a tu portal medico! Por favor ingresa tus <br>
+               para conocer los resultados de tus examenes</p>
+            <div>
+                <form action="" method="get">
+                    <input type="list" name="" id="">
+                    <datalist id="docs">
+                     <option value="Cédula de ciudadania">
+                     <option value="Tarjeta de identidad">
+                     <option value="Cédula de extranjería">
+                     <option value="">
+                     <option value="">
+                    </datalist>
+                    <input type="number" name="" id="">
+                    
+                </form>
+            </div>
+        </div>
+        <div></div>
+    </div>
+</body>
+</html>
